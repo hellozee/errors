d96be8e1c09af3fd1da841b47295c8d7f6cf40c2
Kuntal Majumder <hellozee@disroot.org>
2019-06-30 11:05:01 +0530

    initial test structure

--- errors_test.cc
+++ errors_test.cc
@@ -1,5 +1,12 @@
 #include <iostream>
 #include <gtest/gtest.h>
+#include <errors>
+
+TEST(ErrorsTest, WhatString) {
+    std::string s("just testing");
+    auto e = errors::new_error(s); 
+    EXPECT_EQ(s, e.what());
+}
 
 int main(int argc, char *argv[])
 {
