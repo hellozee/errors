7430c480f70347e2e19260060d0d88b1a8eea67d
Kuntal Majumder <hellozee@disroot.org>
2019-06-30 16:25:35 +0530

    added a simple example

---
+++ examples/multiple_return.cc
@@ -0,0 +1,22 @@
+#include <errors>
+#include <tuple>
+
+auto simple_divide(int a, int b)
+{
+    if(!b){
+        return std::make_tuple(0, errors::new_error("trying to divide by zero"));
+        //can be substituted with {0, errors::new_error("trying to divide by zero")}
+        //for c++17
+    }
+    return std::make_tuple(a/b, errors::nil());
+    //similarly {a/b, errors::nil()}
+}
+
+int main()
+{
+    auto result = simple_divide(4, 0);
+    auto e = std::get<1>(result);
+    if(e != errors::nil()){
+        std::cout << e.what() << std::endl;
+    }
+}
